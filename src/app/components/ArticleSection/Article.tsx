44f76724130570a303f1b1effb6d2a2d72fc2d4e
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-04-27 01:22:36 +0300

    why choose and parallax
    add basic styles to the info section, add "choose" section
    add basic parallax effects

---
+++ src/app/components/ArticleSection/Article.tsx
@@ -0,0 +1,69 @@
+"use client";
+import styles from "./Article.module.scss";
+import { useParallax } from "react-scroll-parallax";
+
+export const Article = () => {
+  const parallaxContainer = useParallax<HTMLDivElement>({
+    translateY: ["805px", "2998px"],
+    startScroll: 760,
+    endScroll: 2994,
+  });
+
+  const businessContainer = useParallax<HTMLDivElement>({
+    translateY: ["0px", "-1655px"],
+    startScroll: 760,
+    endScroll: 2298,
+  });
+
+  const individualContainer = useParallax<HTMLDivElement>({
+    translateY: ["0px", "-1455px"],
+    startScroll: 1140,
+    endScroll: 2994,
+  });
+
+  const reviewsContainer = useParallax<HTMLDivElement>({
+    translateY: ["0px", "-955px"],
+    startScroll: 2080,
+    endScroll: 2970,
+  });
+
+  const startedContainer = useParallax<HTMLDivElement>({
+    translateY: ["0px", "-696px"],
+    startScroll: 3100,
+    endScroll: 3994,
+  });
+
+  return (
+    <section className={`${styles.pin_space} container`}>
+      <div className={styles.wrapper} ref={parallaxContainer.ref}>
+        <article className={styles.article} ref={businessContainer.ref}>
+          01
+        </article>
+        <article
+          ref={individualContainer.ref}
+          className={styles.article}
+          style={{ background: "black", marginTop: "35px" }}
+        >
+          02
+        </article>
+        <article
+          ref={reviewsContainer.ref}
+          className={styles.article}
+          style={{ background: "green", marginTop: "70px" }}
+        >
+          03
+        </article>
+        <article
+          ref={startedContainer.ref}
+          className={styles.article}
+          style={{
+            background: "yellow",
+            marginTop: "105px",
+          }}
+        >
+          04
+        </article>
+      </div>
+    </section>
+  );
+};
